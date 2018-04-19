2b3182ce3cfa79aed5ea9b57d179aa810c35dbbf
Erik Waibel <ejwaibel+github@gmail.com>
2018-04-19 13:16:21 -0700

    Include vue-native-websocket for connection to server

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,7 @@ import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import VueLogger from 'vuejs-logger';
 import App from './App.vue';
+import VueWS from 'vue-native-websocket';
 
 import config from './config';
 import router from './router';
@@ -12,6 +13,10 @@ import store from './store/index';
 
 Vue.use(BootstrapVue);
 Vue.use(VueLogger, config.logOptions);
+Vue.use(VueWS, `ws://${config.host}:3000`, {
+	store,
+	format: 'json',
+});
 
 Vue.prototype.$config = config;
 Vue.config.productionTip = false;
@@ -19,5 +24,5 @@ Vue.config.productionTip = false;
 new Vue({
 	router,
 	store,
-	render: h => h(App),
+	...App
 }).$mount('#app');
