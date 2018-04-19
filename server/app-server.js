c0d8e66ad17c0ef121be39019d6e6f26de348499
Erik Waibel <ejwaibel+github@gmail.com>
2018-04-19 13:15:54 -0700

    Add specific host to CORS config

--- server/app-server.js
+++ server/app-server.js
@@ -71,7 +71,7 @@ let routes = {
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', `http://${config.server}:8181`);
     res.header(
         'Access-Control-Allow-Headers',
         'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With'
