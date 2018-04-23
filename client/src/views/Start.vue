8c09e8b48e1bc5fd87366ec22e6db43a8dd99c7e
Erik Waibel <ejwaibel+github@gmail.com>
2018-04-22 20:35:16 -0700

    Add correct HTML and logic for the Start component

--- client/src/views/Start.vue
+++ client/src/views/Start.vue
@@ -1,15 +1,33 @@
 <template>
-	<div class="container-fluid">
-		<div ui-view="content" class="main-content"></div>
+	<div class="winter">
+		<button class="btn btn-primary button-join-game"
+			v-show="isConnected"
+			@click="joinGame"
+			v-cloak>JOIN GAME</button>
+
+		<div class="alert alert-danger error" role="alert"
+			v-show="!isConnected"
+			v-cloak>Taking a nap. Be back later.</div>
 	</div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 
+// let initialState = {
+// 	isConnected: false
+// }
 export default {
 	name: 'Start',
-	// components: {
-	// 	Start,
-	// },
+	computed: {
+		...mapGetters([
+			'isConnected'
+		]),
+	},
+	methods: {
+		joinGame() {
+			this.$log.debug('join game');
+		}
+	}
 };
 </script>
