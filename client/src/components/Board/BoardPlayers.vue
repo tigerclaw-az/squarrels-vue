3c5ad7e96069f94475697dda5f4bee82368ca1f1
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-01 00:17:07 -0700

    Update logic for BoardPlayers component to addPlayer after game data has loaded

--- client/src/components/Board/BoardPlayers.vue
+++ client/src/components/Board/BoardPlayers.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="sq-players-wrapper">
 		<!-- <webcam channel="webcam"></webcam> -->
-		<div class="sq-players" v-if="players.length >= 2">
+		<div class="sq-players" v-if="playersInGame.length >= 2">
 			<Player
 				v-for="p in playersOrder"
 				:key="p.id"
@@ -18,7 +18,7 @@
 <script>
 // import _ from 'lodash';
 import { mapGetters, mapState } from 'vuex';
-
+import { config } from '@/config';
 import Player from '@/components/Player/Player.vue';
 
 export default {
@@ -26,23 +26,55 @@ export default {
 	components: {
 		Player,
 	},
-	props: ['isGameStarted'],
+	props: ['isGameStarted', 'gameId'],
 	data: function() {
 		return {
 		}
 	},
+	watch: {
+		isGameLoaded() {
+			// TODO: If current player doesn't exist in game.players, then
+			if (this.needMorePlayers() && !this.playerExists()) {
+				this.$store.dispatch({
+					type: 'game/addPlayer',
+					gameId: this.gameId,
+					playerId: this.currentPlayer.id,
+				});
+			}
+		}
+	},
+	created: function() {
+
+	},
 	mounted: function() {
 		this.$log.debug(this);
+
+		// TODO: Remove player from game
+		window.addEventListener('beforeunload', () => {
+			this.$log.debug('unloaded!', this.players);
+		});
 	},
 	computed: {
-		...mapState('game', ['isStarted']),
-		...mapState('players', ['players']),
+		...mapGetters({
+			getPlayerByProp: 'players/get',
+		}),
+		...mapState({
+			currentPlayer: state => state.localPlayer,
+			isGameLoaded: state => state.game.isLoaded,
+			playersInGame: state => state.game.players,
+		}),
 		playersOrder: function() {
-			return this.players;
+			// TODO: Sort by this.currentPlayer.id and then by nextPlayer order
+			return this.playersInGame;
 		},
 	},
 	methods: {
-
+		needMorePlayers: function() {
+			return this.playersInGame.length < config.MAX_PLAYERS;
+		},
+		playerExists: function() {
+			return this.playersInGame.filter(pl => pl === this.currentPlayer.id ).length;
+		},
 	}
 }
 </script>
