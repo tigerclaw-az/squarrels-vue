b77d738cc05791485f7502f4068b03cd70506462
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-01 00:18:54 -0700

    Add generic 'get' method to players module for retrieving specific player(s)

--- client/src/store/modules/players.js
+++ client/src/store/modules/players.js
@@ -13,21 +13,42 @@ const state = {
 };
 
 const getters = {
+	get: (state) => (prop, value, index = false, all = false) => {
+		Vue.$log.debug('get()', prop, value, index);
 
+		let method = index ? 'findIndex' : all ? 'filter' : 'find';
+
+		if (prop) {
+			if (value) {
+				return _[method](state.players, function(o) {
+					return o[prop] === value;
+				});
+			}
+
+			// If a 'value' wasn't given, then we're just looking for the player
+			// where the supplied 'prop' is !null/undefined
+			return _[method](state.players, prop);
+		}
+
+		return state.players;
+	}
 };
 
 const actions = {
 	add({ commit }, plArr) {
-		Vue.$log.debug('plArr->', plArr);
-		return api.players.get(plArr.split(','))
-			.then(res => {
-				if (res.status === 200) {
-					commit('insert', res.data[0]);
-				}
-			})
-			.catch(err => {
-				Vue.$log.error(err);
-			});
+		Vue.$log.debug('add()', plArr);
+
+		if (plArr.length) {
+			return api.players.get(plArr.join(','))
+				.then(res => {
+					if (res.status === 200) {
+						commit('insert', res.data[0]);
+					}
+				})
+				.catch(err => {
+					Vue.$log.error(err);
+				});
+		}
 	},
 
 	insert({ commit }, plObj) {
