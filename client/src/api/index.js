a177406472f11bbaa1607b0970e8896387c082ce
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-02 15:05:38 -0700

    Include the new DecksApi class

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,9 @@
+import DecksApi from './DecksApi';
 import GamesApi from './GamesApi';
 import PlayersApi from './PlayersApi';
 
 export default {
+	decks: new DecksApi(),
 	games: new GamesApi(),
 	players: new PlayersApi(),
 };
