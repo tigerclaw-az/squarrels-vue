095cf94deeee92e5285673448f139295da2981c5
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-02 15:04:52 -0700

    Move scss into separate file for better IDE usage

--- client/src/components/Card/Card.vue
+++ client/src/components/Card/Card.vue
@@ -50,8 +50,7 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss" scoped>
 	@import '~@/components/Card/card.scss';
 
 	.card {
