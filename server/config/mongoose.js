4eba9f3b66b352eaa77107d087c0dbf5c3f87a58
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-03 12:28:45 -0700

    Change debugging output for express server

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose'),
 	mongooseSeed = require('mongoose-seed-db');
 
 mongoose.Promise = require('q').Promise;
-mongoose.set('debug', process.env.LOGLEVEL === 'debug' || process.env.LOGLEVEL === 'trace');
+mongoose.set('debug', process.env.DEBUG_MONGO || false);
 
 module.exports = function() {
 	return mongooseSeed.connect(`mongodb://${process.env.SERVER}/squarrels`);
