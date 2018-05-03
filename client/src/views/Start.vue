6a8ee8f5e9934d1ce8fd2b04b054462a23b5e0ba
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-03 00:06:22 -0700

    Refactor Start component to use state from 'start' module

--- client/src/views/Start.vue
+++ client/src/views/Start.vue
@@ -9,7 +9,7 @@
 			<div v-else
 				class="games-list">
 				<ul>
-					<li v-for="(value, gameId) in games" :key="gameId">
+					<li v-for="(value, gameId) in gamesList" :key="gameId">
 						<router-link :to="{ name: 'game', params: { id: gameId }}"
 							class="btn btn-primary btn-join-game"
 							v-cloak>
@@ -28,10 +28,9 @@
 
 <script>
 import Vue from 'vue';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapState } from 'vuex';
 import Icon from 'vue-awesome/components/Icon';
 
-import api from '@/api/index';
 import EventBus from '@/EventBus';
 
 // FIXME: Make sure games are being added when another player creates the game
@@ -42,10 +41,7 @@ export default {
 	},
 	data: function() {
 		return {
-			games: {},
-			waitCreateGame: false,
-			waitDeleteGame: false,
-			waitLoadGames: false,
+
 		}
 	},
 	watch: {
@@ -66,79 +62,29 @@ export default {
 	},
 	mounted: function() {
 		this.loadGames();
-
-		EventBus.$on('game:create', nuts => {
-			Vue.set(this.games, nuts.id, {});
-			this.waitCreateGame = false;
-		});
-
-		EventBus.$on('game:delete', id => {
-			this.$log.debug('event-game:delete -> ', id);
-			Vue.delete(this.games, id);
-			this.waitDeleteGame = false;
-		})
 	},
 	computed: {
 		...mapGetters([
 			'isConnected'
 		]),
+		...mapState('start', [
+			'gamesList',
+			'waitCreateGame',
+			'waitDeleteGame',
+			'waitLoadGames',
+		]),
 	},
 	methods: {
 		createGame: function() {
-			let vm = this;
-
-			this.waitCreateGame = true;
-
-			api.games.create()
-				.then(res => {
-					let game = res.data;
-
-					vm.$log.debug('Start:games.create', vm, game);
-
-					// The game will be added once we get the
-					// response back from websocket
-					// vm.games.push(game);
-				})
-				.catch(err => {
-					this.waitCreateGame = false;
-					vm.$log.error(err);
-				});
+			this.$store.dispatch('start/createGame');
 		},
 
 		loadGames: function() {
-			let vm = this;
-
-			this.waitLoadGames = true;
-
-			api.games.get()
-				.then(res => {
-					if (res.status === 200) {
-						let gameData = res.data;
-
-						this.waitLoadGames = false;
-
-						for (let game of gameData) {
-							Vue.set(vm.games, game.id, {});
-						}
-					}
-				})
-				.catch(err => {
-					this.waitLoadGames = false;
-					vm.$log.error(err);
-				});
+			this.$store.dispatch('start/loadGames');
 		},
 
 		deleteGame: function(id) {
-			this.waitDeleteGame = true;
-
-			api.games.delete(id)
-				.then(() => {
-					// Actual removal will be done when websocket request comes back
-				})
-				.catch(err => {
-					this.waitDeleteGame = false;
-					this.$log.error(err);
-				});
+			this.$store.dispatch('start/deleteGame', id);
 		},
 	}
 };
