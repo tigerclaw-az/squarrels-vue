e583b01ffbaa5976620cf0b7ba639bcc8d8beac9
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-03 00:04:53 -0700

    Add 'start' module to main store and cleanup code

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -8,49 +8,59 @@ import decks from '@/store/modules/decks';
 import game from '@/store/modules/game';
 import players from '@/store/modules/players';
 import sound from '@/store/modules/sound';
+import start from '@/store/modules/start';
 import websocket from '@/store/modules/websocket';
+
+const modules = {
+	// api,
+	decks,
+	game,
+	players,
+	sound,
+	start,
+	websocket
+};
+
+const state = {
+	isAdmin: false,
+	isLoggedIn: false,
+	localPlayer: {}
+};
+
+const actions = {
+	init({ commit }) {
+		commit('SET_CONFIG', {
+			isAdmin: Vue.$storage.get('isAdmin') || false
+		});
+	},
+
+	checkLogin({ commit }) {
+		return new Promise((resolve, reject) => {
+			let player = Vue.$storage.get('player');
+			if (player) {
+				resolve(player);
+				commit('LOGIN', player);
+			} else {
+				reject('User not logged in');
+			}
+		});
+	}
+};
+
+const mutations = {
+	LOGIN(state, player) {
+		state.isLoggedIn = true;
+		state.localPlayer = player;
+	},
+	SET_CONFIG(state, config) {
+		state.isAdmin = config.isAdmin;
+	},
+};
 
 export default new Vuex.Store({
 	strict: process.env.VUE_APP_STRICT || false,
-	modules: {
-		// api,
-		decks,
-		game,
-		players,
-		sound,
-		websocket
-	},
-	state: {
-		isAdmin: false,
-		isLoggedIn: false,
-		localPlayer: {},
-	},
-	actions: {
-		init({ commit }) {
-			commit('SET_CONFIG', {
-				isAdmin: Vue.$storage.get('isAdmin') || false
-			});
-		},
-
-		checkLogin({ commit }) {
-			return new Promise((resolve, reject) => {
-				let player = Vue.$storage.get('player');
-				if (player) {
-					resolve(player);
-					commit('LOGIN', player);
-				} else {
-					reject('User not logged in');
-				}
-			});
-		}
-	},
-	mutations: {
-		LOGIN(state, player) {
-			state.isLoggedIn = true;
-			state.localPlayer = player;
-		},
-		SET_CONFIG(state, config) {
-			state.isAdmin = config.isAdmin;
-		},
-	},
+	modules,
+	state: state,
+	actions,
+	mutations,
 });
