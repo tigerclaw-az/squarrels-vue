3f91460587f3f2ee2d1633ba3ebf8967ddca5c75
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-05 02:14:52 -0700

    More code formatting from prettier

--- server/models/DeckModel.js
+++ server/models/DeckModel.js
@@ -1,26 +1,31 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
-	DeckSchema = new Schema({
-		deckType: {
-			type: String,
-			required: true
+	DeckSchema = new Schema(
+		{
+			deckType: {
+				type: String,
+				required: true,
+			},
+			cards: [
+				{
+					type: Schema.Types.ObjectId,
+					ref: 'Card',
+				},
+			],
 		},
-		cards: [{
-			type: Schema.Types.ObjectId,
-			ref: 'Card'
-		}]
-	}, {
-		collection: 'decks',
-		timestamps: true,
-		toObject: {
-			virtuals: true
-		},
-		toJSON: {
-			virtuals: true
+		{
+			collection: 'decks',
+			timestamps: true,
+			toObject: {
+				virtuals: true,
+			},
+			toJSON: {
+				virtuals: true,
+			},
 		}
-	});
+	);
 
 module.exports = {
 	schema: DeckSchema,
-	model: mongoose.model('Deck', DeckSchema)
+	model: mongoose.model('Deck', DeckSchema),
 };
