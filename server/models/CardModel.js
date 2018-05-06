ebb95cfe8fb5e7549fe48e38a2a01ec538ad5cb8
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-06 07:20:30 -0700

    Code style cleanup

--- server/models/CardModel.js
+++ server/models/CardModel.js
@@ -1,36 +1,39 @@
-const mongoose = require("mongoose"),
-    config = require("../config/config"),
-    logger = config.logger();
+const mongoose = require('mongoose');
+const config = require('../config/config');
+const logger = config.logger();
 
 var Schema = mongoose.Schema,
-    CardSchema = new Schema({
-        name: {
-            type: String,
-            required: true
-        },
-        amount: {
-            type: Number,
-            required: true
-        },
-        cardType: {
-            type: String,
-            required: true
-        },
-        action: {
-            type: String
-        }
-    }, {
-        collection: 'cards',
-        timestamps: false,
-        toObject: {
-            virtuals: true
-        },
-        toJSON: {
-            virtuals: true
-        }
-    });
+	CardSchema = new Schema(
+		{
+			name: {
+				type: String,
+				required: true,
+			},
+			amount: {
+				type: Number,
+				required: true,
+			},
+			cardType: {
+				type: String,
+				required: true,
+			},
+			action: {
+				type: String,
+			},
+		},
+		{
+			collection: 'cards',
+			timestamps: false,
+			toObject: {
+				virtuals: true,
+			},
+			toJSON: {
+				virtuals: true,
+			},
+		}
+	);
 
 module.exports = {
-    schema: CardSchema,
-    model: mongoose.model('Card', CardSchema)
+	schema: CardSchema,
+	model: mongoose.model('Card', CardSchema),
 };
