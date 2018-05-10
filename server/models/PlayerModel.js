e8e6bd551f12ec6809ad70383494a4be2c5f35f7
Erik Waibel <ejwaibel+github@gmail.com>
2018-05-10 16:09:29 -0700

    Remove isFirstTurn and add hasDrawnCard to PlayerSchema

--- server/models/PlayerModel.js
+++ server/models/PlayerModel.js
@@ -1,56 +1,61 @@
 var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
-	PlayerSchema = new Schema({
-		cardsInHand: {
-			type: [Schema.Types.ObjectId],
-			ref: 'Card',
-			select: false
+	PlayerSchema = new Schema(
+		{
+			cardsInHand: {
+				type: [Schema.Types.ObjectId],
+				ref: 'Card',
+				select: false,
+			},
+			cardsInStorage: [
+				{
+					type: Schema.Types.ObjectId,
+					ref: 'Card',
+				},
+			],
+			img: {
+				type: String,
+			},
+			hasDrawnCard: {
+				type: Boolean,
+				default: false,
+			},
+			isActive: {
+				type: Boolean,
+				default: false,
+			},
+			name: {
+				type: String,
+				required: true,
+				trim: true,
+			},
+			score: {
+				type: Number,
+				default: 0,
+			},
+			sessionId: {
+				type: String,
+				required: true,
+				select: false,
+			},
+			totalCards: {
+				type: Number,
+				default: 0,
+			},
 		},
-		cardsInStorage: [{
-			type: Schema.Types.ObjectId,
-			ref: 'Card'
-		}],
-		img: {
-			type: String
-		},
-		isActive: {
-			type: Boolean,
-			default: false
-		},
-		name: {
-			type: String,
-			required: true,
-			trim: true
-		},
-		score: {
-			type: Number,
-			default: 0
-		},
-		sessionId: {
-			type: String,
-			required: true,
-			select: false
-		},
-		isFirstTurn: {
-			type: Boolean,
-			default: true
-		},
-		totalCards: {
-			type: Number,
-			default: 0
+		{
+			collection: 'players',
+			timestamps: true,
+			toObject: {
+				virtuals: true,
+			},
+			toJSON: {
+				virtuals: true,
+			},
 		}
-	}, {
-		collection: 'players',
-		timestamps: true,
-		toObject: {
-			virtuals: true
-		},
-		toJSON: {
-			virtuals: true
-		}
-	});
+	);
 
 module.exports = {
 	schema: PlayerSchema,
-	model: mongoose.model('Player', PlayerSchema)
+	model: mongoose.model('Player', PlayerSchema),
 };
