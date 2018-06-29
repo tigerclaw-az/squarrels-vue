4ed8a775b40c9cbc786016dad3a6726c60a47199
Erik Waibel <erik@irelo.com>
2018-06-29 10:39:45 -0700

    Fix missing background, dropdown icon, and layout

--- client/src/components/Board/BoardHeader.vue
+++ client/src/components/Board/BoardHeader.vue
@@ -1,25 +1,20 @@
 <template>
 	<b-container fluid class="header">
-		<b-row>
+		<b-row class="align-items-center">
 			<b-col class="header-item game-start">Started @ {{createdAt}}</b-col>
-			<b-col class="header-item game-round">ROUND: {{roundNumber}}</b-col>
-			<b-col class="header-item settings">
-				<b-dropdown id="dropdown-settings" variant="info" no-caret>
+			<b-col class="header-item game-settings">
+				<b-dropdown id="dropdown-settings" variant="info">
 					<template slot="button-content">
-						<span class="icon icon-settings glyphicon glyphicon-cog"></span>
+						<icon name="cog" class="icon icon-settings" label="Settings"></icon>
 					</template>
 					<b-dropdown-item @click="onSettingClick('toggleSound')">
-						<i class="icon icon-mute glyphicon"
-							v-bind:class="{
-								'glyphicon-volume-off': sound.isEnabled,
-								'glyphicon-volume-down': !sound.isEnabled
-							}">
-						</i>
+						<icon :name="getIcon('sound')" class="icon icon-sound" label="Sound"></icon>
 						<span v-if="sound.isEnabled">Disable Sounds</span>
 						<span v-else>Enable Sounds</span>
 					</b-dropdown-item>
 				</b-dropdown>
 			</b-col>
+			<b-col class="header-item game-round">ROUND: {{roundNumber}}</b-col>
 		</b-row>
 	</b-container>
 </template>
@@ -29,10 +24,13 @@ import { mapState } from 'vuex';
 
 import bDropdown from 'bootstrap-vue/es/components/dropdown/dropdown';
 
+import Icon from 'vue-awesome/components/Icon';
+
 export default {
 	name: 'BoardHeader',
 	components: {
 		'b-dropdown': bDropdown,
+		Icon,
 	},
 	props: ['isGameStarted'],
 	data: function() {
@@ -47,6 +45,12 @@ export default {
 		}),
 	},
 	methods: {
+		getIcon: function(name) {
+			switch (name) {
+				case 'sound':
+					return 'volume-' + (this.sound.isEnabled ? 'up' : 'off');
+			}
+		},
 		onSettingClick: function(event) {
 			this.$log.debug(event);
 		},
@@ -54,31 +58,42 @@ export default {
 };
 </script>
 
-<style lang="scss">
-header {
+<style scoped lang="scss">
+// prettier-ignore
+@import "~@/assets/scss/variables";
+
+.header /deep/ {
+	// prettier-ignore
 	background-image: linear-gradient(
-			94deg,
-			rgba(91, 33, 5, 0.1) 0%,
-			rgba(83, 29, 4, 0.32) 23%,
-			rgba(74, 24, 3, 0.41) 47%,
-			rgba(80, 27, 5, 0.44) 70%,
-			rgba(81, 27, 5, 0.59) 74%,
-			rgba(93, 33, 4, 0.2) 83%,
-			rgba(115, 43, 3, 0.5) 100%
-		),
-		linear-gradient(90deg, get-color('cioccolato') 50%, transparent 50%),
-		linear-gradient(
-			90deg,
-			get-color('red-beech') 50%,
-			get-color('carnaby-tan') 50%
-		);
+		94deg,
+		rgba(91, 33, 5, 0.1) 0%,
+		rgba(83, 29, 4, 0.32) 23%,
+		rgba(74, 24, 3, 0.41) 47%,
+		rgba(80, 27, 5, 0.44) 70%,
+		rgba(81, 27, 5, 0.59) 74%,
+		rgba(93, 33, 4, 0.2) 83%,
+		rgba(115, 43, 3, 0.5) 100%
+	),	// prettier-ignore
+	linear-gradient(90deg, get-color("cioccolato") 50%, transparent 50%), // prettier-ignore
+	linear-gradient(
+		90deg,
+		get-color("red-beech") 50%,
+		get-color("carnaby-tan") 50%
+	);
 
 	background-size: 40px 160px, 60px 29px, 27px 27px;
-	color: get-color(white);
-	height: 4rem;
-	line-height: 2;
-	margin: 0 -1rem;
+	color: $white;
 	padding: 0.5rem 1rem;
+
+	.header-item {
+		&.game-settings {
+			text-align: center;
+		}
+
+		&.game-round {
+			text-align: right;
+		}
+	}
 
 	.btn-dropdown {
 		padding: 0.25rem 0.75rem;
