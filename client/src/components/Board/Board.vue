699ec90e97c15922c80c719be300eeb26f6d12e8
Erik Waibel <erik@irelo.com>
2018-08-21 17:54:50 -0700

    Migrate code from BoardPlayers and update layout of players on Board

--- client/src/components/Board/Board.vue
+++ client/src/components/Board/Board.vue
@@ -3,28 +3,49 @@
 		<b-container fluid>
 			<BoardHeader></BoardHeader>
 			<b-row>
-				<b-col>
-					<div class="decks-container">
+				<b-col cols="2">
+					<div class="container_players-left">
+						<Player
+							v-for="(p, index) in opponents"
+							v-if="index % 2 === 0"
+							:key="p.id"
+							:player="p"
+						>
+						</Player>
+					</div>
+				</b-col>
+				<b-col cols="8">
+					<div class="container_decks">
 						<Deck v-if="decksLoaded" v-for="deckId in deckIds" :key="deckId" :id="deckId"></Deck>
+					</div>
+				</b-col>
+				<b-col cols="2">
+					<div class="container_players-right">
+						<Player
+							v-for="(p, index) in opponents"
+							v-if="index % 2 !== 0"
+							:key="p.id"
+							:player="p"
+						>
+						</Player>
 					</div>
 				</b-col>
 			</b-row>
 			<b-row>
-				<b-col>
-					<BoardPlayers :gameId="gameId"></BoardPlayers>
-				</b-col>
+				<Player :player="currentPlayer"></Player>
 			</b-row>
+			<slot name="action"></slot>
 		</b-container>
-		<slot name="action"></slot>
 	</div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import { config } from '@/config';
 
 import BoardHeader from '@/components/Board/BoardHeader.vue';
-import BoardPlayers from '@/components/Board/BoardPlayers.vue';
 import Deck from '@/components/Deck/Deck.vue';
+import Player from '@/components/Player/Player.vue';
 
 export default {
 	name: 'Board',
@@ -42,19 +63,54 @@ export default {
 			required: true,
 		},
 	},
+	watch: {
+		playerIdsInGame: function() {
+			this.$store.dispatch({
+				type: 'players/load',
+				ids: this.playerIdsInGame,
+			});
+		},
+	},
 	mounted: function() {
 		this.$store.dispatch({ type: 'decks/load', ids: this.deckIds });
+
+		if (this.needMorePlayers && !this.playerExists) {
+			this.$store.dispatch({
+				type: 'game/addPlayer',
+				gameId: this.gameId,
+				playerId: this.currentPlayer.id,
+			});
+		}
 	},
 	computed: {
 		...mapState(['isAdmin']),
 		...mapState({
+			allPlayers: state => state.players,
+			currentPlayer: state => state.localPlayer,
 			decks: state => state.decks,
+			playerIdsInGame: state => state.game.playerIds,
 		}),
+		needMorePlayers: function() {
+			return this.playerIdsInGame.length < config.MAX_PLAYERS;
+		},
 		decksLoaded: function() {
 			return this.decks.isLoaded;
 		},
-		needPlayers: function() {
-			return this.playerIds.length < 2;
+		playerExists: function() {
+			return this.playerIdsInGame.filter(
+				pl => pl === this.currentPlayer.id
+			).length;
+		},
+		playersInGame: function() {
+			return _.filter(this.allPlayers, pl =>
+				_.includes(this.playerIdsInGame, pl.id)
+			);
+		},
+		opponents: function() {
+			return _.filter(
+				this.playersInGame,
+				pl => pl.id !== this.currentPlayer.id
+			);
 		},
 	},
 	unload: function() {
@@ -62,11 +118,19 @@ export default {
 	},
 	components: {
 		BoardHeader,
-		BoardPlayers,
 		Deck,
+		Player,
 	},
 };
 </script>
 
 <style lang="scss" scoped>
+.container_decks {
+	align-content: center;
+	align-items: flex-start;
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: center;
+	padding: 0.5rem;
+}
 </style>
