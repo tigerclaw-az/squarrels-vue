196350633ed1a02ea26a01b4181c199ad36adb74
Erik Waibel <erik@irelo.com>
2018-08-28 15:21:27 -0700

    Major changes to rename decks 'Discard' -> 'Hoard' and 'Action' -> 'Discard'

--- client/src/components/Deck/DeckDiscard.vue
+++ client/src/components/Deck/DeckDiscard.vue
@@ -6,17 +6,16 @@
 		}"
 	>
 		<div
-			class="cards-group"
-			:class="{ disabled: isDisabled }"
+			class="cards-group disabled"
 			role="button"
 			v-show="numCards"
-			@click.prevent="onClick"
 		>
 			<transition-group tag="div" class="transition" name="cards-discard">
 				<div
-					v-for="(card, index) in numCards"
+					v-for="(card, index) in cards"
 					:key="index"
-					class="btn-card card blank--"
+					:class="`action--${card.name}`"
+					class="btn-card card"
 				>
 				</div>
 			</transition-group>
@@ -25,8 +24,6 @@
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
-
 export default {
 	name: 'DeckDiscard',
 	props: {
@@ -39,77 +36,11 @@ export default {
 			required: true,
 		},
 	},
-	data: function() {
-		return {
-			maxClicks: 4,
-			tooManyClicks: false,
-		};
-	},
-	mounted: function() {
-		// this.$store.dispatch('decks/load', this.id);
-	},
-	computed: {
-		...mapGetters({
-			canDrawCard: 'players/canDrawCard',
-			myPlayer: 'players/getMyPlayer',
-		}),
-		...mapState({
-			actionCard: state => state.game.actionCard,
-		}),
-		canHoard: function() {
-			if (!this.actionCard) {
-				return false;
-			}
-
-			return !this.myPlayer.isActive && this.actionCard.name === 'hoard';
-		},
-		isDisabled: function() {
-			return this.tooManyClicks || !this.actionCard;
-		},
-	},
-	methods: {
-		collectHoard: function() {
-			if (this.canHoard) {
-				this.$socket.sendObj({
-					action: 'hoard',
-					player: this.myPlayer,
-				});
-
-				return true;
-			}
-
-			if (this.myPlayer.cardsInHand.length) {
-				this.$store.dispatch('players/removeHighCard', this.myPlayer);
-			}
-		},
-		onClick: function() {
-			if (this.actionCard) {
-				this.collectHoard();
-			} else {
-				if (this.maxClicks >= 0) {
-					this.$toasted.info(
-						`STOP THAT! Only ${this.maxClicks} clicks LEFT!`
-					);
-					this.maxClicks--;
-				} else {
-					this.$toasted.error(
-						'You have been banned from collecting the Hoard!'
-					);
-					this.tooManyClicks = true;
-				}
-			}
-		},
-	},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-// prettier-ignore
-@import "~@/assets/scss/variables";
-// prettier-ignore
-@import "~@/../node_modules/bootstrap/scss/mixins/breakpoints";
-
 .card {
 	top: 0;
 
