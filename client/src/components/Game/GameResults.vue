f0b980507c0b8dcce371cb5bb6ffff5ef501d0e5
Erik Waibel <erik@irelo.com>
2018-08-28 11:41:26 -0700

    Initial commit of GameResults component

---
+++ client/src/components/Game/GameResults.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="game-results">
+		<p>TEST</p>
+		<slot name="newGame"></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'GameResults',
+	props: {
+		gameId: {
+			type: String,
+			required: true,
+		},
+		playerIds: {
+			type: Array,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+#game-results {
+	position: absolute;
+	z-index: 120;
+}
+</style>
