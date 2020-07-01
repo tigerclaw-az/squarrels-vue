77fa99e526a1793e3f560905329b1abc4d4fc1c7
Erik Waibel <ejwaibel+github@gmail.com>
2020-06-30 19:00:46 -0700

    Update config to allow for image urls in bootstrap components

--- client/vue.config.js
+++ client/vue.config.js
@@ -21,6 +21,27 @@ module.exports = {
 			.plugin('moment')
 			.use(webpack.IgnorePlugin, [/^\.\/locale$/, /moment$/]);
 
+		config.module
+			.rule('vue')
+			.use('vue-loader')
+			.loader('vue-loader')
+			.tap(options => {
+				options.transformAssetUrls = {
+					'img': 'src',
+					'image': 'xlink:href',
+					'b-avatar': 'src',
+					'b-img': 'src',
+					'b-img-lazy': ['src', 'blank-src'],
+					'b-card': 'img-src',
+					'b-card-img': 'src',
+					'b-card-img-lazy': ['src', 'blank-src'],
+					'b-carousel-slide': 'img-src',
+					'b-embed': 'src',
+				};
+
+				return options;
+			});
+
 		/* Leave this in case we need other locales later */
 		// .use(webpack.ContextReplacementPlugin, [
 		// 	/moment[\/\\]locale$/,
