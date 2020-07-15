2ef8c0a2915ea8f972672495f5c052a5831e0bbc
Erik Waibel <ejwaibel+github@gmail.com>
2020-07-15 11:48:59 -0700

    Remove debugging

--- client/src/store/modules/sound.js
+++ client/src/store/modules/sound.js
@@ -5,21 +5,11 @@ import audiospriteConfig from '@/assets/sounds/audiosprite.json';
 import mutationTypes from '@/store/mutation-types';
 
 const howlConfig = Object.assign({}, audiospriteConfig, {
-	// pool: 1,
-	onload: function() {
-		Vue.$log.info('audio::onload', this.state());
-	},
 	onloaderror: function(error) {
-		Vue.$log.info('audio::onload', this.state(), error);
+		Vue.$log.error('audio::onloaderror', this.state(), error);
 	},
 	onplayerror: function(id, err) {
 		Vue.$log.error('audio::onplayerror -> ', id, err);
-	},
-	onplay: function(id) {
-		Vue.$log.info('audio::onplay -> ', id);
-	},
-	onend: id => {
-		Vue.$log.info('audio::onend -> ', id);
 	},
 });
 
@@ -40,9 +30,7 @@ const actions = {
 		this._vm.$log.debug('audio::play', state, audio, name);
 
 		if (state.isEnabled) {
-			const id = audio.play(name);
-
-			this._vm.$log.info('audio::id -> ', id);
+			audio.play(name);
 		}
 	},
 	toggle: ({ commit }) => {
